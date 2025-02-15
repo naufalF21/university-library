4f3789ce006bfec2c5d2bfd7854cb14673c75b8e
Naufal F <naufalfirman098@gmail.com>
2025-02-15 21:35:26 +0700

    feat: added next-auth for authentication

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,7 +14,6 @@ import { Button } from "@/components/ui/button";
 import {
 	Form,
 	FormControl,
-	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
@@ -24,6 +23,8 @@ import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { FIELD_NAMES, FIELD_TYPES } from "@/constants";
 import FileUpload from "./FileUpload";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 interface Props<T extends FieldValues> {
 	schema: ZodType<T>;
@@ -33,6 +34,7 @@ interface Props<T extends FieldValues> {
 }
 
 const AuthForm = <T extends FieldValues>({ type, schema, defaultValues, onSubmit }: Props<T>) => {
+	const router = useRouter();
 	const isSignIn = type === "SIGN_IN";
 
 	const form: UseFormReturn<T> = useForm({
@@ -40,7 +42,26 @@ const AuthForm = <T extends FieldValues>({ type, schema, defaultValues, onSubmit
 		defaultValues: defaultValues as DefaultValues<T>,
 	});
 
-	const handleSubmit: SubmitHandler<T> = async (data) => {};
+	const handleSubmit: SubmitHandler<T> = async (data) => {
+		const result = await onSubmit(data);
+
+		if (result.success) {
+			toast({
+				title: "Success",
+				description: isSignIn
+					? "You have successfully signed in."
+					: "You have successfully signed up.",
+			});
+
+			router.push("/");
+		} else {
+			toast({
+				title: `Error ${isSignIn ? "signing in" : "signing up"}`,
+				description: result.error ?? "An error occurred.",
+				variant: "destructive",
+			});
+		}
+	};
 
 	return (
 		<div className="flex flex-col gap-4">

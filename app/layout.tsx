691dcaa53393095a3911024afda44318887dedfc
Naufal F <naufalfirman098@gmail.com>
2025-02-15 21:38:56 +0700

    feat: added next-auth for authentication (#1)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-
+import { SessionProvider } from "next-auth/react";
 import localFont from "next/font/local";
 import { ReactNode } from "react";
+import { auth } from "@/auth";
 
 const ibmPlexSans = localFont({
 	src: [
@@ -46,14 +47,18 @@ export const metadata: Metadata = {
 	description: "BookWise is a book borrowing university library management solution.",
 };
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+	const session = await auth();
+
 	return (
 		<html lang="en">
-			<body className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}>
-				{children}
+			<SessionProvider session={session}>
+				<body className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}>
+					{children}
 
-				<Toaster />
-			</body>
+					<Toaster />
+				</body>
+			</SessionProvider>
 		</html>
 	);
 };
